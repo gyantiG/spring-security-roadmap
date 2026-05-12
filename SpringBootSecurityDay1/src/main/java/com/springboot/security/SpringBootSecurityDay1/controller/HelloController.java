9037744cd816ec2990f711b25602f1fc65770995
gyantiG <gyantigupta2015@gmail.com>
2026-05-12 06:35:42 +0530

    third commit for day1

--- SpringBootSecurityDay1/src/main/java/com/springboot/security/SpringBootSecurityDay1/controller/HelloController.java
+++ SpringBootSecurityDay1/src/main/java/com/springboot/security/SpringBootSecurityDay1/controller/HelloController.java
@@ -5,9 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello day1";
+	}
+
+	// task 1
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Spring Boot";
+	}
+
+	// task 2
+	@GetMapping("/about")
+	public String about() {
+		return "Learning spring security step by step";
 	}
 
 }
