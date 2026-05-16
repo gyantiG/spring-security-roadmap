686a1526c216e8398939e4d5ed6ea63775b8b0d7
gyantiG <gyantigupta2015@gmail.com>
2026-05-16 18:22:15 +0530

    second commit

---
+++ SpringBootSecurityDay2/src/main/java/com/springboot/security/SpringBootSecurityDay2/SpringBootSecurityDay2Application.java
@@ -0,0 +1,14 @@
+package com.springboot.security.SpringBootSecurityDay2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityDay2Application.class, args);
+		System.out.println("day 2");
+	}
+
+}
